d8b46a4713ff7abfa37e11452100baf0ce00c1c9
Jan Henrik Weinstock <jan@mwa.re>
2023-06-19 19:50:45 +0200

    Added -fno-var-tracking to fix gcc error on ubuntu 22.04

--- Sanitizer.cmake
+++ Sanitizer.cmake
@@ -8,18 +8,33 @@
  #                                                                            #
  ##############################################################################
 
+# Common Sanitizer flags
+set(sani_c_flags "-fno-omit-frame-pointer -g -O1")
+set(sani_cxx_flags "-fno-omit-frame-pointer -g -O1")
+set(sani_linker_flags "-fno-omit-frame-pointer -g -O1")
+
+if (CMAKE_C_COMPILER_ID STREQUAL "GNU" AND
+    CMAKE_C_COMPILER_VERSION VERSION_LESS "12.0.0")
+    set(sani_c_flags "${sani_c_flags} -fno-var-tracking-assignments")
+endif()
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND 
+    CMAKE_CXX_COMPILER_VERSION VERSION_LESS "12.0.0")
+    set(sani_cxx_flags "${sani_cxx_flags} -fno-var-tracking-assignments")
+endif()
+
 # Address Sanitizer
 set(CMAKE_C_FLAGS_ASAN
-    "-fsanitize=address -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=address ${sani_c_flags}" CACHE STRING
     "Flags used by the C++ compiler during ASAN builds." FORCE)
 set(CMAKE_CXX_FLAGS_ASAN
-    "-fsanitize=address -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=address ${sani_cxx_flags}" CACHE STRING
     "Flags used by the C compiler during ASAN builds." FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_ASAN
-    "-fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=address ${sani_linker_flags}" CACHE STRING
     "Flags used for linking binaries during ASAN builds." FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS_ASAN
-    "-fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=address ${sani_linker_flags}" CACHE STRING
     "Flags used by the shared libraries linker during ASAN builds." FORCE)
 
 mark_as_advanced(CMAKE_C_FLAGS_ASAN CMAKE_CXX_FLAGS_ASAN
@@ -28,16 +43,16 @@ mark_as_advanced(CMAKE_C_FLAGS_ASAN CMAKE_CXX_FLAGS_ASAN
 
 # Thread Sanitizer
 set(CMAKE_C_FLAGS_TSAN
-    "-fsanitize=thread -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=thread ${sani_c_flags}" CACHE STRING
     "Flags used by the C++ compiler during TSAN builds." FORCE)
 set(CMAKE_CXX_FLAGS_TSAN
-    "-fsanitize=thread -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=thread ${sani_cxx_flags}" CACHE STRING
     "Flags used by the C compiler during TSAN builds." FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_TSAN
-    "-fsanitize=thread -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=thread ${sani_linker_flags}" CACHE STRING
     "Flags used for linking binaries during TSAN builds." FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS_TSAN
-    "-fsanitize=thread -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=thread ${sani_linker_flags}" CACHE STRING
     "Flags used by the shared libraries linker during TSAN builds." FORCE)
 
 mark_as_advanced(CMAKE_C_FLAGS_TSAN CMAKE_CXX_FLAGS_TSAN
@@ -46,16 +61,16 @@ mark_as_advanced(CMAKE_C_FLAGS_TSAN CMAKE_CXX_FLAGS_TSAN
 
 # Undefined Behavior Sanitizer
 set(CMAKE_C_FLAGS_UBSAN
-    "-fsanitize=undefined -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=undefined ${sani_c_flags}" CACHE STRING
     "Flags used by the C++ compiler during UBSAN builds." FORCE)
 set(CMAKE_CXX_FLAGS_UBSAN
-    "-fsanitize=undefined -fno-omit-frame-pointer -g -O1" CACHE STRING
+    "-fsanitize=undefined ${sani_cxx_flags}" CACHE STRING
     "Flags used by the C compiler during UBSAN builds." FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_UBSAN
-    "-fsanitize=undefined -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=undefined ${sani_linker_flags}" CACHE STRING
     "Flags used for linking binaries during UBSAN builds." FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS_UBSAN
-    "-fsanitize=undefined -fno-omit-frame-pointer" CACHE STRING
+    "-fsanitize=undefined ${sani_linker_flags}" CACHE STRING
     "Flags used by the shared libraries linker during UBSAN builds." FORCE)
 
 mark_as_advanced(CMAKE_C_FLAGS_UBSAN CMAKE_CXX_FLAGS_UBSAN
