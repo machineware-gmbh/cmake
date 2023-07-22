52aac1c01170ceccee6682cb884459a7f7c9851a
Jan Henrik Weinstock <jan@mwa.re>
2023-07-22 21:36:47 +0200

    Added target_link_whole_archives helper

---
+++ LinkWhole.cmake
@@ -0,0 +1,39 @@
+ ##############################################################################
+ #                                                                            #
+ # Copyright (C) 2023 MachineWare GmbH                                        #
+ # All Rights Reserved                                                        #
+ #                                                                            #
+ # This is work is licensed under the terms described in the LICENSE file     #
+ # found in the root directory of this source tree.                           #
+ #                                                                            #
+ ##############################################################################
+
+function(do_target_link_whole_archives target visibility lib)
+    if(MSVC)
+        target_link_libraries(${target} ${visibility} "/WHOLEARCHIVE:${lib}")
+    elseif(APPLE)
+        target_link_libraries(${target} ${visibility} -Wl,-force_load ${lib})
+    else()
+        target_link_libraries(${target} ${visibility} -Wl,--whole-archive ${lib} -Wl,--no-whole-archive)
+    endif()
+endfunction()
+
+function(target_link_whole_archives target)
+    cmake_parse_arguments(WHOLE "" "" "PUBLIC;PRIVATE;INTERFACE" ${ARGN})
+
+    foreach(lib IN LISTS WHOLE_PRIVATE)
+        do_target_link_whole_archives(${target} PRIVATE ${lib})
+    endforeach()
+ 
+    foreach(lib IN LISTS WHOLE_PUBLIC)
+        do_target_link_whole_archives(${target} PUBLIC ${lib})
+    endforeach()
+
+    foreach(lib IN LISTS WHOLE_INTERFACE)
+        do_target_link_whole_archives(${target} INTERFACE ${lib})
+    endforeach()
+
+    foreach(lib IN LISTS WHOLE_UNPARSED_ARGUMENTS)
+        do_target_link_whole_archives(${target} "" ${lib})
+    endforeach()
+endfunction()
